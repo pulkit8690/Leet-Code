4f35652f4f655fdf5f98d74257213f071aae5872
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-06-09 11:07:54 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (22.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,32 +1,25 @@
 class Solution {
     public double myPow(double x, int n) {
-        // Handle the special case where n is Integer.MIN_VALUE
-        long N = n;
-        
-        // If n is negative, invert x and use the positive counterpart of n
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+        long N=n;
+        if(N<0)
+        {
+            x=1/x;
+            N=-N;
         }
-        
-        return myPowHelper(x, N);
+        return myPow2(x,N);
     }
-
-    private double myPowHelper(double x, long n) {
-        // Base case: any number to the power of 0 is 1
-        if (n == 0) {
+    private double myPow2(double x, long N)
+    {
+        if(N==0)
+        {
             return 1;
         }
-        
-        // Recursive step: compute half power
-        double halfPow = myPowHelper(x, n / 2);
-        double halfPowSq = halfPow * halfPow;
-        
-        // If n is odd, we need to multiply by x one more time
-        if (n % 2 != 0) {
-            halfPowSq *= x;
+        double halfPow=myPow2(x,N/2);
+        double halfPow_Sq=halfPow*halfPow;
+        if(N%2!=0)
+        {
+            halfPow_Sq=x*halfPow_Sq;
         }
-        
-        return halfPowSq;
+        return halfPow_Sq;
     }
 };

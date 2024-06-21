ab9560025c7d9a9e230ec01140ad44c2d13a9cfd
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-06-21 18:20:04 +0530

    Time: 20 ms (18.93%), Space: 55.7 MB (76.42%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++)
-        {
-            if(i==nums.length-1)
-            {
-                return false;
-            }
-            if((nums[i] ^ nums[i+1])==0)
-            {
-                return true;
-            }
+        Arrays.sort(nums); 
+        for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] == nums[i+1]) {
+                    return true;
+                }
         }
         return false;
+
     }
 }

549a672445423b51226aefd42e9ab321041683e0
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-07-07 12:39:24 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (8.89%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,10 +1,6 @@
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
         int ans= numBottles;
-        if(numExchange==0)
-        {
-            return ans;
-        }
         while((numBottles/numExchange)>0)
         {
             ans= ans+(numBottles/numExchange);

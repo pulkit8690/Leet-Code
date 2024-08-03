2974982a46c577bbad37525a26a183a0da6aedd3
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-08-03 10:04:13 +0530

    Time: 11 ms (8.75%), Space: 44.5 MB (18.11%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,27 +1,18 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer,Integer> m1 = new HashMap<>();
-        HashMap<Integer,Integer> m2 = new HashMap<>();
-        for(int i=0;i<target.length;i++)
-        {
-            m1.put(target[i],m1.getOrDefault(target[i],0)+1);
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+        
+        for (int num : target) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-        for(int i=0;i<arr.length;i++)
-        {
-            m2.put(arr[i],m2.getOrDefault(arr[i],0)+1);
-        }
-        for(Integer key : m1.keySet())
-        {
-            if(!m2.containsKey(key))
-            {
-                return false;
-            }else{
-                if(m2.get(key)!=m1.get(key))
-                {
-                    return false;
-                }
+        
+        for (int num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) - 1);
+            if (countMap.get(num) == 0) {
+                countMap.remove(num);
             }
         }
-        return true;
+        
+        return countMap.isEmpty();
     }
 }

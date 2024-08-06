d82ca04ef026f585e3e05614ebbeb4c2cc4ffc80
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-08-06 11:02:14 +0530

    Time: 0 ms (100.00%), Space: 42 MB (21.75%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,17 +1,12 @@
 class Solution {
     public int minimumPushes(String word) {
-        HashMap<Integer,Integer> map= new HashMap<>();
-        int res=0;
-        int assign_key=2;
-        for(char ch : word.toCharArray())
+       int res=0;
+        for(int i=0;i<word.length();i++)
         {
-            if(assign_key>9)
-            {
-                assign_key=2;
-            }
-            map.put(assign_key,map.getOrDefault(assign_key,0)+1);
-            res+=map.get(assign_key);
-            assign_key++;
+            if(i/8==0) res+=1;
+            else if(i/8==1) res+=2;
+            else if(i/8==2) res+=3;
+            else res+=4;
         }
         return res;
     }

cd4edd44767112fbf6d13baa54530d9d3766f7ed
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-08-17 18:58:14 +0530

    Time: 11 ms (28.69%), Space: 45.4 MB (80.48%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int n=score.length;
+        int arr[][]= new int[n][2];
+        for(int i=0;i<n;i++)
+        {
+            arr[i][0]=i;
+            arr[i][1]=score[i];
+        }
+        Arrays.sort(arr, Comparator.comparingDouble(o->o[1]));
+        String ans[]= new String[n];
+        int rank=1;
+        for(int i=1;i<=n;i++)
+        {
+            if(rank==1)
+            {
+                ans[arr[n-i][0]]="Gold Medal";
+                rank++;
+            }
+            else if(rank==2)
+            {
+                ans[arr[n-i][0]]="Silver Medal";
+                rank++;
+            }
+            else if(rank==3)
+            {
+                ans[arr[n-i][0]]="Bronze Medal";
+                rank++;
+            }
+            else{
+                ans[arr[n-i][0]]=Integer.toString(rank);
+                rank++;
+            }
+        }
+        return ans;
+        
+    }
+}

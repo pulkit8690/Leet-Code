ea155c3054a271cd539f63e63fd76aa63132eda4
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-08-22 00:19:43 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (97.45%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,19 +1,18 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n= cost.length;
-        int dp[]= new int [n];
-        Arrays.fill(dp,-1);
-        return Math.min(helper(cost,0,n,dp), helper(cost,1,n,dp));
+        return helper(cost,n);
     }
-    public int helper(int arr[], int i, int n,int dp[])
+    public int helper(int cost[], int n)
     {
-        if(i>=n) return 0; 
-        if(dp[i]!=-1)
-        {
-            return dp[i];
+        int dp[]= new int[n];
+        if(n==1) return cost[0];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+         for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
-        int step1= arr[i]+helper(arr,i+1,n,dp);
-        int step2= arr[i]+helper(arr,i+2,n,dp);
-        return dp[i]= Math.min(step1,step2);
+        
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
 }

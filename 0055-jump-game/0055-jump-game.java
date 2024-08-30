69be2c443de44db690fe880bfde8bb59103fdd1e
Pulkit Arora <103959073+pulkit8690@users.noreply.github.com>
2024-08-30 10:01:17 +0530

    Time: 419 ms (5.37%), Space: 45.7 MB (30.69%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n= nums.length-1;
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(0,nums,n,dp)==1;
+        int dp[]= new int[n+1];
+        Arrays.fill(dp, -1);
+        return helper(nums,0,n,dp)==1;
     }
-    public int  helper(int i, int arr[], int n, int dp[])
+    public int helper(int arr[], int i,int n, int dp[])
     {
         if(i==n) return 1;
         if(arr[i]==0 || i>n) return 0;
@@ -13,9 +13,9 @@ class Solution {
         {
             return dp[i];
         }
-        for(int j=1;j<=arr[i];j++)
+        for(int k=1;k<=arr[i];k++)
         {
-            if( helper(i+j,arr,n,dp)==1)
+            if(helper(arr,i+k,n,dp)==1)
             {
                 return dp[i]=1;
             }
